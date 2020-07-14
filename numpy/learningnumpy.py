71607b473aee8982ae46f851f785841f8b416ccc
iammidhun <midhunmohanputhur@gmail.com>
2020-07-14 08:02:34 +0530

    multiple array creation

--- numpy/learningnumpy.py
+++ numpy/learningnumpy.py
@@ -1,3 +1,11 @@
 import numpy  as np
 my_list = [1,2,3,4,5] #first create a list
 my_array = np.array(my_list) #this will return array
+
+#multi dimension 
+mylist2 = [11,22,33,44,55]
+my_list2 = [my_list,mylist2] #combining 2  lists of my_list and mylist2   
+my_array =  np.array(my_list2)
+
+array([[ 1,  2,  3,  4,  5],
+      [11, 22, 33, 44, 55]]) #this will be the output
