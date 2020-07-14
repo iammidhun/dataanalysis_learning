814404586b4b5b227811a40a340d870b71a00d6f
iammidhun <midhunmohanputhur@gmail.com>
2020-07-14 08:21:53 +0530

    zero array creation

--- numpy/learningnumpy.py
+++ numpy/learningnumpy.py
@@ -9,3 +9,6 @@ my_array =  np.array(my_list2)
 
 array([[ 1,  2,  3,  4,  5],
       [11, 22, 33, 44, 55]]) #this will be the output
+
+np.zeros(4) #this will return array of elements with zero and shape of (1,4)
+array([0., 0., 0., 0.]) #output .dtype of the output will be float
